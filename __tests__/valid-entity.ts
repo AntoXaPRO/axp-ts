cc7bf11d1c204cf905370377c658d9fead300f33
AntoXa PRO <info@antoxa.pro>
2021-12-13 21:39:16 +0300

    Fix valid entity (change convertByFields)

--- __tests__/valid-entity.ts
+++ __tests__/valid-entity.ts
@@ -1,34 +1,37 @@
-import { object, string, number } from 'yup';
+import { object, string, number, date, SchemaOf } from 'yup';
 import { ErrorItem, ValidEntity } from '../src';
 
 type TTestValid = {
 	name: string;
-	test: string;
+	birthday?: Date;
 	age?: number;
 };
 
-const schema = object({
+const schema: SchemaOf<TTestValid> = object({
 	name: string().trim().required(),
-	test: string().trim().required(),
 	age: number().default(10),
+	birthday: date(),
 });
 
-const obj = { name: 'Field name ', test: 'Test ' };
-const objTest = { name: 'Field name', age: 10, test: 'Test' };
+const obj = { name: ' Field name ', birthday: '' };
+const objTest = { name: 'Field name', age: 10 };
 
 describe('Valid entity', () => {
 	test('Create instance', () => {
 		const entity = new ValidEntity<TTestValid>(obj, schema);
-		expect(entity.obj).toEqual(objTest);
+
+		expect(entity.obj.name).toBe(objTest.name);
+		expect(entity.obj.age).toBe(undefined);
+		expect(entity.obj.birthday).toBe(undefined);
 		expect(entity.isValid()).toBe(true);
-		expect(entity.errors.length).toBe(0);
+		expect(entity.obj.age).toBe(10);
 	});
 
 	test('Error messages', () => {
 		const entity = new ValidEntity(null, schema);
 		expect(entity.isValid()).toBe(false);
 		expect(entity.errors[0]).toBeInstanceOf(ErrorItem);
-		expect(entity.errors.length).toBe(2);
+		expect(entity.errors.length).toBe(1);
 		expect(entity.errors[0].code).toBe('name');
 	});
 });
--- src/valid-entity.ts
+++ src/valid-entity.ts
@@ -1,4 +1,5 @@
-import { ValidationError, SchemaOf } from 'yup';
+import { ValidationError, ObjectSchema } from 'yup';
+import { ObjectShape } from 'yup/lib/object';
 
 import { ErrorItem } from '.';
 
@@ -12,11 +13,11 @@ interface IValidEntity<T> {
 
 class ValidEntity<T> implements IValidEntity<T> {
 	_errors: any = {};
-	_schema: SchemaOf<T>;
+	_schema: ObjectSchema<ObjectShape>;
 
 	obj: T;
 
-	constructor(obj: T, schema: SchemaOf<T>) {
+	constructor(obj: any, schema: ObjectSchema<ObjectShape>) {
 		this._schema = schema;
 		this.obj = this.convertByFields(obj || {});
 	}
@@ -34,17 +35,25 @@ class ValidEntity<T> implements IValidEntity<T> {
 		return arr;
 	}
 
+	private _setErrors(ex: ValidationError) {
+		if (ex.inner) {
+			ex.inner.forEach((e) => (this._errors[e.path] = e.message));
+		}
+	}
+
 	/**
 	 * Валидация по схеме.
 	 */
 	isValid(): boolean {
 		this._errors = {};
 		try {
-			this._schema.validateSync(this.obj, { abortEarly: false });
+			this.obj = <T>this._schema.validateSync(this.obj, {
+				abortEarly: false,
+				stripUnknown: true,
+			});
 			return true;
 		} catch (ex) {
-			const error = <ValidationError>ex;
-			error.inner.forEach((e) => (this._errors[e.path] = e.message));
+			this._setErrors(ex);
 			return false;
 		}
 	}
@@ -54,7 +63,21 @@ class ValidEntity<T> implements IValidEntity<T> {
 	 * @param obj - Объект для конвертации.
 	 */
 	convertByFields(obj: any = this.obj): T {
-		return <T>this._schema.cast(obj, { stripUnknown: true });
+		let result: T = <T>{};
+		Object.keys(this._schema.fields).forEach((key) => {
+			try {
+				const value = obj[key];
+				if (value) {
+					result[key] = this._schema.fields[key].cast(value, {
+						stripUnknown: true,
+						assert: true,
+					});
+				}
+			} catch (ex) {
+				this._setErrors(ex);
+			}
+		});
+		return result;
 	}
 
 	/**
