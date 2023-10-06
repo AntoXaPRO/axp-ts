17f05dd9e214fc5ac99980a74a994f2be127da30
AntoXa PRO <info@antoxa.pro>
2023-10-06 12:43:46 +0300

    fix FindFilter

--- src/requests/find-filter.ts
+++ src/requests/find-filter.ts
@@ -22,8 +22,8 @@ export type TQuery = z.infer<typeof querySchema>
  * Класс для работы с запросами (для удобства).
  */
 export type TFindFilter<T extends TQuery> = {
-	obj: Omit<T, 'page' | 'limit' | 'sort'>
-	pagination: TPaginationQuery
+	obj?: Omit<T, 'page' | 'limit' | 'sort'>
+	pagination?: TPaginationQuery
 	sort?: string
 }
 
@@ -31,8 +31,8 @@ export type TFindFilter<T extends TQuery> = {
  * Класс для работы с запросами (для удобства).
  */
 export class FindFilter<T extends TQuery> implements TFindFilter<T> {
-	obj: Omit<T, 'page' | 'limit' | 'sort'>
-	pagination: TPaginationQuery
+	obj?: Omit<T, 'page' | 'limit' | 'sort'>
+	pagination?: TPaginationQuery
 	sort?: string
 
 	constructor(query?: T) {
@@ -73,5 +73,38 @@ export class FindFilter<T extends TQuery> implements TFindFilter<T> {
 
 	isEqual(filters: TFindFilter<T>[]) {
 		return isEqual([this, ...filters])
+	}
+
+	toString(filter?: TFindFilter<TQuery>, opt?: { base?: string }): string {
+		let url = opt?.base?.replace(/[?]$/, '') || ''
+
+		if (filter) {
+			const query: string[] = []
+
+			// Params and Pagination.
+			for (const [key, val] of Object.entries({
+				...filter.obj,
+				...filter.pagination
+			})) {
+				if (val) {
+					const keyEncode = encodeURIComponent(key)
+					const valEncode = encodeURIComponent(val)
+					query.push(keyEncode + '=' + valEncode)
+				}
+			}
+
+			// Sort.
+			if (filter.sort) {
+				try {
+					query.push('sort=' + encodeURIComponent(filter.sort))
+				} catch (e) {}
+			}
+
+			if (query.length) {
+				url += '?' + query.join('&')
+			}
+		}
+
+		return url
 	}
 }
